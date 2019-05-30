ef63dbb51510604e39fa374b74d066eca7703351
Dace Howard <dace61@gmail.com>
2019-05-30 18:06:11 +0930

    menu updates and smooth scroll

--- js/app.js
+++ js/app.js
@@ -11,56 +11,20 @@ $(document).ready(function(){
   })
 });
 
-// Smooth anchor scrolling
+// Smooth anchor scrolling + offset landing
 
-
-// Offset anchor landing
-
-function offsetAnchor() {
-  if (location.hash.length !== 0) {
-    window.scrollTo(window.scrollX, window.scrollY - 90);
-  }
-}
-$(document).on('click', 'a[href^="#"]', function(event) {
-  window.setTimeout(function() {
-    offsetAnchor();
-  }, 0);
+$('a').click(function(){
+  $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top -60
+  }, 400);
+  return false;
 });
-
-window.setTimeout(offsetAnchor, 0);
 
 // Navigation close upon clicking menu item or scrolling
 
-if(answer == true){
-  $('.navbar-toggler').trigger('click');
-}
-
-
-var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos < currentScrollPos) {
-    $('.navbar-toggler').trigger('click');
-  } else {
-    
+  var navDropButton = $('button.navbar-toggler');
+  if (navDropButton.attr('aria-expanded') === 'true') {
+    navDropButton.trigger('click');
   }
-  prevScrollpos = currentScrollPos;
 }
-
-// Old hide nav bar function
-
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos < currentScrollPos) {
-//     $("#nav1").addClass('shrink-nav');
-//     $("#nav2").addClass('shrink-nav');
-//   } else {
-//     $("#nav1").removeClass('shrink-nav');
-//     $("#nav2").removeClass('shrink-nav');
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
-
-// Attempt at smooth scrolling
