de4d5656d286026cfbb52a94937ea12f988c8032
Dace <dace61@gmail.com>
2019-05-30 11:52:23 +0930

    removed nav hide

--- js/app.js
+++ js/app.js
@@ -31,15 +31,36 @@ window.setTimeout(offsetAnchor, 0);
 
 // Navigation close upon clicking menu item or scrolling
 
+if(answer == true){
+  $('.navbar-toggler').trigger('click');
+}
+
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos < currentScrollPos) {
-    $("#nav1").addClass('shrink-nav');
-    $("#nav2").addClass('shrink-nav');
+    $('.navbar-toggler').trigger('click');
   } else {
-    $("#nav1").removeClass('shrink-nav');
-    $("#nav2").removeClass('shrink-nav');
+    
   }
   prevScrollpos = currentScrollPos;
 }
+
+// Old hide nav bar function
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos < currentScrollPos) {
+//     $("#nav1").addClass('shrink-nav');
+//     $("#nav2").addClass('shrink-nav');
+//   } else {
+//     $("#nav1").removeClass('shrink-nav');
+//     $("#nav2").removeClass('shrink-nav');
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+// Attempt at smooth scrolling
